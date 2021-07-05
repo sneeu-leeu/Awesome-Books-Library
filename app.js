c81fc4f90cb9b616ebead117d96c8747a8d143b9
Mwila Kaunda <mbkcust@gmail.com>
2021-07-05 16:46:35 +0200

    Add logic for local storage

--- app.js
+++ app.js
@@ -3,23 +3,35 @@ var list = [];
 function g(j) {
     return document.getElementById(j);
 }
+function stack(templateString) {
+    var template = document.createElement("template");
+    template.innerHTML = templateString.trim();
+    document.getElementById("books").appendChild(template.content.firstChild);
+}
 function addBook() {
     var title = g("t").value;
     var author = g("a").value;
-    var template = document.createElement("template");
-    template.innerHTML = ("<div id=\"" + (i + title) + "\">\n    <p>" + title + "</p>\n    <p>" + author + "</p>\n    <button id=\"" + (i + title) + "\" value=\"" + i + "\" onclick=\"removeBook(this.id, this.value)\">Remove</button>\n  </div>\n  <hr>").trim();
-    document.getElementById("books").appendChild(template.content.firstChild);
+    stack("<div id=\"" + (i + title) + "\">\n      <p>" + title + "</p>\n      <p>" + author + "</p>\n      <button id=\"" + (i + title) + "\" value=\"" + i + "\" onclick=\"removeBook(this.id, this.value)\">Remove</button>\n    </div>\n    <hr>");
     var book = {
         author: author,
         title: title
     };
     list[i] = book;
     i++;
-    console.log(list);
+    localStorage.list = list;
 }
 function removeBook(id, p) {
     var position = parseInt(p);
     list.splice(position, 1);
-    document.getElementById(id).remove;
+    document.getElementById(id).remove();
+    localStorage.list = list;
 }
+function loadPrev() {
+    var mList = localStorage.list;
+    for (var j = 0; j < mList.length; j++) {
+        var book = mList[j];
+        stack("<div id=\"" + (j + book.title) + "\">\n        <p>" + book.title + "</p>\n        <p>" + book.author + "</p>\n        <button id=\"" + (j + book.title) + "\" value=\"" + j + "\" onclick=\"removeBook(this.id, this.value)\">Remove</button>\n      </div>\n      <hr>");
+    }
+}
+window.onload = loadPrev();
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -5,19 +5,23 @@ function g(j: string): HTMLInputElement {
   return document.getElementById(j) as HTMLInputElement
 }
 
+function stack(templateString: string) {
+  var template = document.createElement("template")
+  template.innerHTML = templateString.trim()
+  document.getElementById("books").appendChild(template.content.firstChild)
+}
+
 function addBook() {
   var title = g("t").value
   var author = g("a").value
 
-  var template = document.createElement("template")
-  template.innerHTML = `<div id="${i+title}">
-    <p>${title}</p>
-    <p>${author}</p>
-    <button id="${i+title}" value="${i}" onclick="removeBook(this.id, this.value)">Remove</button>
-  </div>
-  <hr>`.trim()
-
-  document.getElementById("books").appendChild(template.content.firstChild)
+  stack(`<div id="${i + title}">
+      <p>${title}</p>
+      <p>${author}</p>
+      <button id="${i + title}" value="${i}" onclick="removeBook(this.id, this.value)">Remove</button>
+    </div>
+    <hr>`
+  )
 
   var book = {
     author: author,
@@ -27,12 +31,30 @@ function addBook() {
   list[i] = book
   i++
 
-  console.log(list)
+  localStorage.list = list
 }
 
-function removeBook(id: string ,p: string) {
+function removeBook(id: string, p: string) {
   let position = parseInt(p);
   list.splice(position, 1);
 
-  document.getElementById(id).remove
+  document.getElementById(id).remove()
+  localStorage.list = list
 }
+
+function loadPrev() {
+  let mList = localStorage.list as number[]
+
+  for (let j = 0; j < mList.length; j++) {
+    let book = mList[j] as any
+    stack(`<div id="${j + book.title}">
+        <p>${book.title}</p>
+        <p>${book.author}</p>
+        <button id="${j + book.title}" value="${j}" onclick="removeBook(this.id, this.value)">Remove</button>
+      </div>
+      <hr>`
+    )
+  }
+}
+
+window.onload = loadPrev()
