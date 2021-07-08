f0c849319965d584d6f88683dd68c70e16a1676f
sneeu-leeu <sa070197@gmail.com>
2021-07-08 15:16:18 +0200

    Fix *

--- js/handler.js
+++ js/handler.js
@@ -5,7 +5,7 @@ book.loadPrev();
 
 document.getElementById('add_book').addEventListener('click', () => {
   book.addBook();
-})
+});
 
 function target(element) {
   book.removeBook(element.id, element.value);
