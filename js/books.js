e4ea8d178ab7ddfca7ea635ea978263834240715
sneeu-leeu <sa070197@gmail.com>
2021-07-09 15:49:41 +0200

    Fix the linters

--- js/books.js
+++ js/books.js
@@ -13,35 +13,35 @@ function target(element) {
 
 function display_c() {
   var refresh = 1000; // Refresh rate in milli seconds
-  mytime = setTimeout('display_ct()', refresh)
+  mytime = setTimeout('display_ct()', refresh);
 }
 
 function display_ct() {
-  var x = new Date()
+  var x = new Date();
   var x1 = x.toUTCString();// changing the display to UTC string
   document.getElementById('ct').innerHTML = x1;
   tt = display_c();
 }
 
-display_ct()
+display_ct();
 
 function _switch(target) {
   switch (target) {
-    case 'toList':
-      document.getElementById('list').style.display = "block"
-      document.getElementById('foo_book').style.display = "none"
-      document.getElementById('contact').style.display = "none"
-      break
     case 'toAdd':
-      document.getElementById('list').style.display = "none"
-      document.getElementById('foo_book').style.display = "block"
-      document.getElementById('contact').style.display = "none"
-      break
+      document.getElementById('list').style.display = 'none';
+      document.getElementById('foo_book').style.display = 'block';
+      document.getElementById('contact').style.display = 'none';
+      break;
     case 'toContact':
-      document.getElementById('list').style.display = "none"
-      document.getElementById('foo_book').style.display = "none"
-      document.getElementById('contact').style.display = "block"
-      break
+      document.getElementById('list').style.display = 'none';
+      document.getElementById('foo_book').style.display = 'none';
+      document.getElementById('contact').style.display = 'block';
+      break;
+    default:
+      document.getElementById('list').style.display = 'block';
+      document.getElementById('foo_book').style.display = 'none';
+      document.getElementById('contact').style.display = 'none';
+    break;
   }
 }
 /* eslint-enable no-unused-vars */
